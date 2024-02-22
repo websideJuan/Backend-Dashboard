1fb9c199586d0eabe066e6d0a35ca6700f4a282b
juanHerrera <portador1051@gmail.com>
2024-02-22 13:44:16 -0300

    backend application deployment 0.0.4

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express()
 connectDB()
 app.use(express.json())
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'http://localhost:3000',
   credentials: true
 }))
 app.use(cookieParser())
